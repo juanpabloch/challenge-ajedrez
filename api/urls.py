45c36876f359a9aa268bc2af13460d1206f50da4
Juan Pablo Choternasty <juanpablochoter@gmail.com>
2025-05-15 11:14:57 -0300

    se borro endpoint de prueba

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('api/csrf/', views.get_csrf_token, name='api_csrf'),
     path('api/register/', views.RegisterAPI.as_view(), name='api_register'),
     path('api/login/',  views.LoginAPI.as_view(),  name='api_login'),
     path('api/logout/', views.LogoutAPI.as_view(), name='api_logout'),
